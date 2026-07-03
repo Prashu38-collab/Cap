3dd62a184e88f942120dff817be4487c1653c9ba
prietyMaharjan <priety2080-0131@iimscollege.edu.np>
2026-07-03 19:58:33 +0545

    done

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -62,17 +62,29 @@ export default function MyProfile() {
 
      < Navbar />
 
+    
       <section className="profile-hero">
-        <img src="\images\contactus.png" alt="Boudhanath Stupa Nepal" className="profile-hero-image" />
-        <div className="profile-hero-overlay" />
-        <div className="profile-hero-copy">
-          <a href="#profile-card" className="profile-hero-button">My Profile</a>
-        </div>
-      </section>
+  <img
+    src="/images/contactus.png"
+    alt="Boudhanath Stupa Nepal"
+    className="profile-hero-image"
+  />
 
-      {/* MAIN CONTENT CONTAINER */}
-      <p className='text'>Welcome Back, Traveler</p>
+  <div className="profile-hero-overlay" />
 
+  <div className="profile-hero-copy">
+    <h1 className="hero-title">
+      Every Journey Tells a Story
+    </h1>
+
+<p className="hero-quote">
+  "Travel isn't always about the destination, it's about the memories
+  you create along the way."
+</p>
+
+
+  </div>
+</section>
       <main className="profile-main">
         
         {/* PERSONAL INFORMATION CARD */}
